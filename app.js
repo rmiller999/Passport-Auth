e06bd273dff928664bf26f36449dffa2c43fda47
Reid Miller <reidmiller999@gmail.com>
2020-02-20 21:33:01 -0800

    Created user model and configured passport

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const express = require("express");
+const express = require("express"),
+      mongoose = require("mongoose"),
+      passport = require("passport"),
+      bodyParser = require("body-parser"),
+      User = require("./models/user"),
+      localStrategy = require("passport-local"),
+      passportLocalMongoose = require("passport-local-mongoose");
+mongoose.connect("mongodb://localhost:27017/auth_demo", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
+
+app.use(require("express-session")({
+  secret: "Expecto Patronum",
+  resave: false,
+  saveUninitialized: false
+}));
 app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req,res) {
   res.render("home")
 });
 
-
+app.get("/secret", function(req,res) {
+  res.render("secret")
+});
 
 
 
