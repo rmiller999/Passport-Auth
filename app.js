4d12016a542e58afc446a9b2bc1b5a7c287614bd
Reid Miller <reidmiller999@gmail.com>
2020-02-20 21:57:20 -0800

    created login routes

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.urlencoded({extended: true}));
 
+passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -55,6 +56,21 @@ app.post("/register", function(req,res) {
     }
   })
 });
+
+//LOGIN ROUTES
+
+//render login form
+app.get("/login", function(req,res) {
+  res.render("login")
+});
+
+//login logic
+app.post("/login", passport.authenticate("local" , {
+  successRedirect: "/secret",
+  failureRedirect: "/login"
+}) ,function(req,res) {
+
+})
 
 app.listen(3000, () => {
   console.log("Server Started!!!")
