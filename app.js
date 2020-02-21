ae78b7e8932a6fc9c4e0a8cfa5d8e87bb4067805
Reid Miller <reidmiller999@gmail.com>
2020-02-20 21:48:00 -0800

    created register routes

--- app.js
+++ app.js
@@ -17,9 +17,13 @@ app.use(require("express-session")({
 app.set('view engine', 'ejs');
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: true}));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+//=============================
+//ROUTES
 
 app.get("/", function(req,res) {
   res.render("home")
@@ -29,7 +33,28 @@ app.get("/secret", function(req,res) {
   res.render("secret")
 });
 
+//AUTH ROUTES
 
+//show sign up form
+app.get("/register", function(req,res) {
+  res.render("register")
+});
+
+//handle user sign up
+app.post("/register", function(req,res) {
+  req.body.username
+  req.body.password
+  User.register(new User({username: req.body.username}), req.body.password, function(err,user) {
+    if(err) {
+      console.log(err)
+      res.render('register')
+    } else {
+      passport.authenticate("local")(req,res, function() {
+        res.redirect("/secret")
+      })
+    }
+  })
+});
 
 app.listen(3000, () => {
   console.log("Server Started!!!")
